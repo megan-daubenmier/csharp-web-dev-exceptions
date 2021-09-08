7eaa5d54ec18bb3a719864db9196d9010621f159
Megan Daubenmier <megan.daubenmier@gmail.com>
2021-09-08 01:10:17 -0500

    Chapter 11 exercises complete.

--- csharp-web-dev-exceptions/Program.cs
+++ csharp-web-dev-exceptions/Program.cs
@@ -7,21 +7,53 @@ namespace csharp_web_dev_lsn9exceptions
 {
     class Program
     {
-        /*
+
         static double Divide(double x, double y)
         {
-            // Write your code here!
+            if (y == 0)
+            {
+                throw new ArgumentOutOfRangeException("The denominator is 0.");
+            }
+            else
+            {
+                return x / y;
+            }
         }
 
         static int CheckFileExtension(string fileName)
         {
-            // Write your code here!
+            int numPoints;
+            if (fileName == "" || fileName == null)
+            {
+                throw new ArgumentNullException("Please enter a non-null file name.");
+            }
+            else if (fileName.Substring(fileName.Length - 3) != ".cs")
+            {
+                numPoints = 0;
+            }
+            else
+            {
+                numPoints = 1;
+            }
+
+            return numPoints;
         }
-        */
+
 
         static void Main(string[] args)
         {
             // Test out your Divide() function here!
+            double divisionResult = 0;
+            try
+            {
+                divisionResult = Divide(1, 0);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Please enter a non-zero denominator.");
+            }
+
+            //Console.WriteLine(divisionResult);
 
             // Test out your CheckFileExtension() function here!
             Dictionary<string, string> students = new Dictionary<string, string>();
@@ -30,7 +62,17 @@ namespace csharp_web_dev_lsn9exceptions
             students.Add("Elizabeth", "MyCode.cs");
             students.Add("Stefanie", "CoolProgram.cs");
 
-
+            foreach (KeyValuePair<string, string> student in students)
+            {
+                try
+                {
+                    Console.WriteLine(student.Key + ": " + CheckFileExtension(student.Value));
+                }
+                catch (ArgumentNullException n)
+                {
+                    Console.WriteLine("Please provide a non-null file name.");
+                }
+            }
         }
     }
 }
